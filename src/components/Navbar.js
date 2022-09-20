628cd7881c9c5c5945af3729ea19d809d8bfa0fb
Kirill Lyamzin <kiryanoff88@gmail.com>
2022-09-20 19:42:28 -0400

    Added search functionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,15 @@
 import { Link } from 'react-router-dom';
 import './Navbar.css';
+import SearchBar from './SearchBar';
 
 export default function Navbar() {
   return (
     <div className="navbar">
       <nav>
         <Link className="brand" to="/">
-          <h1>Cheesecake Avenue</h1>
+          <h1>Cake Ave.</h1>
         </Link>
+        <SearchBar></SearchBar>
         <Link to="/create">Create Recipe</Link>
       </nav>
     </div>
