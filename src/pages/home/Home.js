07861401d3043a53467a887c806f139220804652
Kirill Lyamzin <kiryanoff88@gmail.com>
2022-09-20 16:21:20 -0400

    finished recipes page added create recipe page

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import './Home.css';
 import { useFetch } from '../../hooks/useFetch';
+import RecipeList from '../../components/RecipeList';
 const url = 'http://localhost:3000/recipes';
 
 export default function Home() {
   const { data, isPending, error } = useFetch(url);
   return (
     <div className="home">
-      {error && <p className="error">{error.message}</p>}
+      {error && <p className="error">{error}</p>}
       {isPending && <p className="loading">Loading...</p>}
-      {data && data.map((recipe) => <h2 key={recipe.id}>{recipe.title}</h2>)}
+      {data && <RecipeList recipes={data} />}
     </div>
   );
 }
--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -1,6 +1,36 @@
-import React from 'react';
+import { useFetch } from '../../hooks/useFetch';
+// import { useEffect } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
 import './Recipe.css';
 
 export default function Recipe() {
-  return <div>Recipe</div>;
+  const { id } = useParams(); // this will give us params.id from the url
+  const url = 'http://localhost:3000/recipes/' + id;
+  const { data: recipe, isPending, error } = useFetch(url);
+  // const history = useHistory();
+  // useEffect(() => {
+  //   if (error) {
+  //     setTimeout(() => {
+  //       history.push('/');
+  //     }, 2000);
+  //   }
+  // }, [error, history]);
+  return (
+    <div className="recipe">
+      {error && <p className="error">{error}</p>}
+      {isPending && <p className="loading">Loading...</p>}
+      {recipe && (
+        <>
+          <h2 className="page-title">{recipe.title}</h2>
+          <p>Takes {recipe.cookingTime} to cook.</p>
+          <ul>
+            {recipe.ingredients.map((ing) => (
+              <li key={ing}>{ing}</li>
+            ))}
+          </ul>
+          <p className="method">{recipe.method}</p>
+        </>
+      )}
+    </div>
+  );
 }
